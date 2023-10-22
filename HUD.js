a05a661213a4e807c92985e2062e87a3393a9c82
Medina, Juan Gabriel <129141347+MedinaJuanGabriel@users.noreply.github.com>
2023-10-21 23:53:31 -0300

    Update HUD.js

--- HUD.js
+++ HUD.js
@@ -1,33 +1,32 @@
 class HUD {
-  constructor() {
-    
+  constructor(puntos) {
+    this.puntosGenerales= puntos;
     this.xCorazones = width - 50.10+7; // Posición de los corazones en el borde superior derecho
     this.yCorazones = 10;
-    this.xpuntos=10*50;
-    this.ypuntos=10*4;
-    this.puntaje = 0; // Inicializa el puntaje en 0
+    this.xflauta=10*49;
+    this.yflauta=10*4;
     this.cargaHUD();
   }
 cargaHUD(){
     this.corazonLleno = loadImage('assets/corazonlleno.png'); // Imagen del corazón lleno
     this.corazonVacio = loadImage('assets/corazonvacio.png'); // Imagen del corazón vacío
-    this.puntos = loadImage('assets/puntos.png');
+    this.flautaDecoracion = loadImage('assets/puntos.png');
 }
-  mostrar(vidas) {
-    for (let i = 0; i < vidas; i++) {
+mostrar(vidas, puntosGenerales) {
+  for (let i = 0; i < 3; i++) {
+    if (i < vidas) {
       image(this.corazonLleno, i * 50, 10, 40, 40);
+    } else {
+      image(this.corazonVacio, i * 50, 10, 40, 40);
     }
-
-    for (let i = vidas; i < 3; i++) {
-      image(this.corazonVacio, i * 30, 10, 20, 20);
-    }
-
-    image(this.puntos, this.xpuntos, this.ypuntos, 70, 25);
-    fill(255, 255, 0); // Establece el color del texto en amarillo
-  textSize(24); // Tamaño del texto
-  text("Puntos: " + this.puntaje, this.xpuntos - 20, this.ypuntos - 5);
   }
- actualizarPuntaje(cantidad) {
-    this.puntaje += cantidad;
-  }
+
+  // Dibuja la imagen de la flauta decorativa
+  image(this.flautaDecoracion, this.xflauta, this.yflauta, 100, 25);
+
+  // Dibuja el contador de puntos en la imagen de la flauta
+  fill(255, 255, 0);
+  textSize(24);
+  text("Puntos: " + puntosGenerales, this.xflauta - 5, this.yflauta + 1);
+}
 }
