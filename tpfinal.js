294d525ad44a8d5008f55f9306d4813cf3e7442d
Medina, Juan Gabriel <129141347+MedinaJuanGabriel@users.noreply.github.com>
2023-12-04 19:03:56 -0300

    Update tpfinal.js

--- tpfinal.js
+++ tpfinal.js
@@ -1,18 +1,23 @@
+//Declaracion inicial de la aventura grafica
 let ag;
 
+//Setup
 function setup() {
+  //Tamaño del lienzo
   let canvas = createCanvas(600, 600);
+  //Declaracion de la clase aventura grafica
   ag = new AventuraGrafica();
 }
 
+//Draw
 function draw() {
-  
+ //declarando los metodos en la clase aventura grafica
   ag.actualizar();
   ag.dibujarjuego();
- 
   ag.dibujar();
 }
 
 function keyPressed() {
+   //declarando el metodo en la clase aventura grafica
   ag.teclaPresionada(keyCode);
 }
