4abd4157efaf0f4cec74ad634fdd8804ef3bcf5f
Medina, Juan Gabriel <129141347+MedinaJuanGabriel@users.noreply.github.com>
2023-12-04 19:50:19 -0300

    Update class_boton.js

--- class_boton.js
+++ class_boton.js
@@ -1,17 +1,17 @@
 class Boton {
   constructor(x, y, ancho, alto, texto, accion) {
+    //Parametros del boton:
     this.x = x;
     this.y = y;
     this.ancho = ancho;
     this.alto = alto;
     this.texto = texto;
     this.accion = accion;
-    this.clicado = false;
-    
+    this.clicado = false;  
   }
-
+   //Visualizacion del boton
    mostrar() {
-       fill(169,173,128,170);
+    fill(169,173,128,170);
     rect(this.x, this.y, this.ancho, this.alto);
     fill(0);
     textSize(18);
@@ -19,7 +19,7 @@ class Boton {
     text(this.texto, this.x + this.ancho / 2, this.y + this.alto / 2);
   }
 
-
+//funcionalidad del boton
    clicEnBoton() {
   if (mouseIsPressed &&
     mouseX > this.x && mouseX < this.x + this.ancho &&
